34920c86202504325036559f22923ca3896ae5a5
Carhuavilca Velasquez <vero03velasquez@gmail.com>
2025-05-21 21:43:13 -0300

    fix: bugs SearchBar.jsx

--- trabajo-practico-4/src/components/SearchBar.jsx
+++ trabajo-practico-4/src/components/SearchBar.jsx
@@ -1,60 +1,23 @@
-import React, {useEffect, useState} from "react";
+import { useState } from "react";
 
-const search =({NombreBuscar, setNombreBuscar}) =>{
-    return <input 
-    type="text"
-    value={NombreBuscar}
-    onChange ={(e)=>setNombreBuscar(e.target.value)}
-    
-    />
+const SearchBar = ({ filtrarProductos }) => {
+    const [busqueda, setBusqueda] = useState("");
 
-} 
+    const handleChange = (e) => {
+        setBusqueda(e.target.value);
+        filtrarProductos(e.target.value); // Actualiza la búsqueda en App.jsx
+    };
 
-useEffect(()  =>{
+    return (
+        <div>
+            <input 
+                type="text" 
+                placeholder="Buscar por ID o descripción..." 
+                value={busqueda} 
+                onChange={handleChange} 
+            />
+        </div>
+    );
+};
 
-    setFiltro(
-        productos.filter((productos) => productos.toLowercase().
-        includes(search.toLowercase()))
-    )
-        
-}, [nombreBuscar, productos])
-
-const lista =({productos}) =>{
-
-    if (productos.lenght===0)return <div>Sin resultados </div>
-
-    return <ul>
-        {
-            productos.map((producto, index) =>(
-                <li key={index}>{producto}</li>
-            ))
-        }
-    </ul>
-
-}
-
-
-
-
-const searchBar =() =>{
-
-    const[nombreBuscar, setNombreBuscar] = usedState('');
-
-    const[filtro, setFiltro] = usedState([]);
-
-    const[productos, setproductos] = useState(
-        [
-            "prod1",
-            "prod2",
-            "prod3",
-        ]);
-    
-
-    return <div>
-        <search {...{nombreBuscar, setNombreBuscar }}/>
-        <lista {...{productos: filtered}}/>
-    </div>
-
-}
-
-export default searchBar
+export default SearchBar;
